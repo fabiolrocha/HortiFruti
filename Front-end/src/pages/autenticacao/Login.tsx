4f0aedaf83dc8895dc6fd99016afdfa4548f474b
Fábio Luiz Cardoso Laranjeria Rocha <fabiolrocha2013@gmail.com>
2025-06-03 23:27:08 -0300

    finalizando as primeiras alterações

--- Front-end/src/pages/autenticacao/Login.tsx
+++ Front-end/src/pages/autenticacao/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
-import "./Login.css";
+import "../style.css";
 
 const Login: React.FC = () => {
   // Estado para armazenar o username e password
